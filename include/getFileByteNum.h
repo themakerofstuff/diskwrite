dee861b3487778ccb0508b9485bea804726c0168
themakerofstuff <khanalman061@gmail.com>
2022-07-04 19:29:51 +0100

    First commit of 'diskwrite-cli', version 1.0

---
+++ include/getFileByteNum.h
@@ -0,0 +1,7 @@
+
+#ifndef GETFILEBYTENUM_C
+#define GETFILEBYTENUM_C
+
+unsigned long get_file_byte_num(const char * filename)
+
+#endif
---
+++ src/cli/functions/getFileByteNum.c
@@ -0,0 +1,7 @@
+#include <malloc.h>
+
+unsigned long get_file_byte_num(const char* filename) {
+    FILE *fp = fopen(filename, "r");
+    fseek(fp, 0, SEEK_END);
+    return ftell(fp);
+}
